62637ff5f8dabafd6bb677da3ff84600651fe646
Cibele Martins <93346472+CibeleMartins@users.noreply.github.com>
2022-04-14 18:18:40 -0300

    :package: feat: Implementou a pages Home
    Implementações css na pages Homes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,17 +16,20 @@ return (
       
       <div className={styles.flag}>
         
-
-      
-
       </div>
 
       
       <div className={styles.text}>
+
         <h1>Conectando pessoas pela arte</h1>
         <p>Crie sua conta e receba pré-atendimentos de sessões de tatuagem e solicitações de orçamento  de todo o Brasil</p>
-
+        
       </div>
+
+      <div className={styles.flag_artist}>
+        
+      </div>
+  
 
   </main>
 )
