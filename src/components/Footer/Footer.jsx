5e637953abfadb4d8ff179327f0a122c1c182ecd
LagAntunes <lag151617@gmail.com>
2022-04-16 20:35:17 -0300

    arrumando rotas com react router 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,42 +9,28 @@ import {FaInstagram} from 'react-icons/fa';
 const Footer = () => {
     
   return (
-    
-           <footer className={styles.footer}>
+    <footer className={styles.footer}>
+      <section className={styles.section}>
+        <p> Entre em contato, </p>
+        <p> Adoraríamos ouvir você! </p>
+      </section>
 
-           <section className={styles.section}>
-            <p> Entre em contato, </p>
-            <p> Adoraríamos ouvir você! </p>
-            </section>
+      <section className={styles.sectionRedesSociaisParagrafo}>  
+        <div className={styles.sectionRedesSociais}>  
+          <FaFacebookF className={styles.icone}/>
+          <FaWhatsapp className={styles.icone}/>
+          <FaInstagram className={styles.icone}/>
+        </div>
 
-        
-           
-           <div     className={styles.sectionRedesSociaisParagrafo}>  
-            
-            <section className={styles.sectionRedesSociais}>  
-        
-           <FaFacebookF style={{margin:"5px"}}/> 
-           <FaWhatsapp  style={{margin:"5px"}}/>
-           <FaInstagram style={{margin:"5px"}}/>
-           </section>
-
-           <p> Art Studio 2022 </p>
-           </div> 
-           
-
-           <section className={styles.sectionInformacoes}>                
-            <p> Tattoo Art Studio </p>
-            <p> Com sede em São Paulo </p>
-            <p> artTstudio@gmail.com </p>
-            </section>
-            
-           
-
-         
-
-          </footer>
-    
-    
+        <p>Art Studio <span>2022</span></p>
+      </section> 
+      
+      <section className={styles.sectionInformacoes}>                
+        <p> Tattoo Art Studio </p>
+        <p> Com sede em São Paulo </p>
+        <p> artTstudio@gmail.com </p>
+      </section>
+    </footer>
   );
 };
 
