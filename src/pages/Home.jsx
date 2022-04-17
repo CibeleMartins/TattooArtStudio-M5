2650df59ed530ea57cbe79d18d378d16df2ea549
Cibele Martins <93346472+CibeleMartins@users.noreply.github.com>
2022-04-17 19:07:37 -0300

    :dizzy: feat: Inicia animação em css
    Implementou uma pequena animação ao primeiro parágrafo da Homes.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import styles from './Home.module.css';
 import Button from '../components/Button/Button'
 import styles_b from '../components/Button/Button.module.css'
+// import Rellax from 'rellax';
 
-
-
+// const rellax = 
+//   new Rellax('.rellax', {
+//     speed: -7,
+//     center: false,
+//     wrapper: null,
+//     round: true,
+//     vertical: true,
+//     horizontal: false
+//   })
 
 const Home = ({text}) => {
+
+
 
 return (
 
@@ -24,10 +34,12 @@ return (
 
         </div>
 
+       
         <div className={styles.text}>
+        
           <h1>Conectando pessoas pela arte</h1>
-          <p>Crie sua conta e receba pré-atendimentos de sessões de tatuagem e solicitações de orçamento  de todo o Brasil</p>
-          
+          <p className={styles.type_animation}>Crie sua conta e receba pré-atendimentos de sessões de tatuagem e solicitações de orçamento de todo o Brasil</p>
+       
         </div>
 
         <div className={styles.flag_artist}></div>
@@ -49,10 +61,12 @@ return (
           <p className={styles.big_p}>Conheça um pouco da nossa arte</p>
 
           <div style={{display: "flex", flexDirection: "column", alignItems:"flex-end"}}>
-          <Button text="Faça um orçamento" className={styles_b.button_hp}/>
+            <Button text="Faça um orçamento" className={styles_b.button_hp}/>
           </div>
           
         </div>
+
+
 
     </main>
   )
