6afcba9d046d9db478f9a75ba081d1bd4451ef5c
gilson <sgvidros.sacadas@gmail.com>
2022-04-25 19:48:16 -0300

    adicionando itens de formulário

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,6 +1,7 @@
 import styles from "./Contato.module.css"
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
+import ButtonPages from "../components/Button/ButtonPages"
 
 function Contato() {
     return(
@@ -10,17 +11,26 @@ function Contato() {
             
             
             <div className={styles.divCadastro}>
-                <h2>Preencha o formulário abaixo e aproveite <span>benefícios exclusivos</span></h2>
+                <h2> Envie suas duvida ou sugestões</h2>
                 <form className={styles.displayForm}>
 
                     <div>
-                        <label htmlFor="email">Cadastre seu email:</label><br/>
+                        <label htmlFor="email">Informe seu email:</label><br/>
                         <input type="mail" placeholder="Digite seu email aqui..." id="email" required/><br/>
                     </div>
 
                     <div>
-                        <label htmlFor="tel">Cadastre seu número (WhatsApp):</label><br/>
+                        <label htmlFor="tel">Informe seu número (WhatsApp):</label><br/>
                         <input type="tel" placeholder="Digite seu email aqui..." id="tel" required/><br/>
+                    </div>
+
+                    <div>
+                        <label htmlFor="email">Envie sua mensagem:</label><br/>
+                         <textarea></textarea>
+                    </div>
+
+                    <div>
+                     <ButtonPages text="Enviar mensagem"/>
                     </div>
 
                                     
