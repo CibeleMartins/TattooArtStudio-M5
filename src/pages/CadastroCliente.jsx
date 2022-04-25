05e5d79448365514775a32102d73bc8fd366b4c1
LagAntunes <lag151617@gmail.com>
2022-04-25 13:07:57 -0300

    adicionando função de checangem de input na página cadastro cliente

--- src/pages/CadastroCliente.jsx
+++ src/pages/CadastroCliente.jsx
@@ -9,6 +9,18 @@ import ButtonPages from "../components/Button/ButtonPages"
 
 
 function CadastroCliente() {
+    
+    function checaInput(e) {
+        let nome = document.getElementById("nome")
+        let senha = document.getElementById("senha")
+        let email = document.getElementById("email")
+        let tel = document.getElementById("tel")
+
+        if(nome.value != "" && senha.value != "" && email.value != "" && tel.value != "") {
+            alert("Cliente cadastrado com sucesso!")
+        }
+    }
+
     return(
         <div className={styles.background}>
             <Header/>
@@ -18,27 +30,27 @@ function CadastroCliente() {
                 <form className={styles.displayForm}>
                     <div>
                         <label htmlFor="nome">Cadastre seu nome:</label><br/>
-                        <input type="text" placeholder="Digite seu nome aqui..." id="nome"/><br/>
+                        <input type="text" placeholder="Digite seu nome aqui..." id="nome" required/><br/>
                     </div>
 
                     <div>
                         <label htmlFor="senha">Cadastre sua senha:</label><br/>
-                        <input type="text" placeholder="Digite sua senha aqui..." id="senha"/><br/>
+                        <input type="text" placeholder="Digite sua senha aqui..." id="senha" required/><br/>
                     </div>
 
                     <div>
                         <label htmlFor="email">Cadastre seu email:</label><br/>
-                        <input type="mail" placeholder="Digite seu email aqui..." id="email"/><br/>
+                        <input type="mail" placeholder="Digite seu email aqui..." id="email" required/><br/>
                     </div>
 
                     <div>
                         <label htmlFor="tel">Cadastre seu número (WhatsApp):</label><br/>
-                        <input type="tel" placeholder="Digite seu email aqui..." id="tel"/><br/>
+                        <input type="tel" placeholder="Digite seu email aqui..." id="tel" required/><br/>
                     </div>
 
                     <div className={styles.divBotao}>
                         <div>
-                            <ButtonPages text="Cadastrar cliente"/>
+                            <ButtonPages text="Cadastrar cliente" onClick={checaInput}/>
                         </div>
                     </div>
                 </form>
