a3622b7d58acaf1581ffcf0f17fc57be56efedf2
gilson <sgvidros.sacadas@gmail.com>
2022-04-25 19:33:11 -0300

    adicionando itens no formulario

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -7,9 +7,26 @@ function Contato() {
         <> 
             <Header/>
 
-            <div className={styles.container}>
-                <h1>gilson</h1>
+            
+            
+            <div className={styles.divCadastro}>
+                <h2>Preencha o formulário abaixo e aproveite <span>benefícios exclusivos</span></h2>
+                <form className={styles.displayForm}>
+
+                    <div>
+                        <label htmlFor="email">Cadastre seu email:</label><br/>
+                        <input type="mail" placeholder="Digite seu email aqui..." id="email" required/><br/>
+                    </div>
+
+                    <div>
+                        <label htmlFor="tel">Cadastre seu número (WhatsApp):</label><br/>
+                        <input type="tel" placeholder="Digite seu email aqui..." id="tel" required/><br/>
+                    </div>
+
+                                    
+                </form>
             </div>
+
 
             <Footer/>
         </>
