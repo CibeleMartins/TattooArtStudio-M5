f4f7c050d3051eb0ab42e96184025a0e589f6434
Cibele Martins <93346472+CibeleMartins@users.noreply.github.com>
2022-04-26 16:52:18 -0300

    :package: feat: corrige identação pages Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-
 import styles from './Home.module.css';
 import Button from '../components/Button/Button';
 import styles_b from '../components/Button/Button.module.css';
 import logo from '../assets/imagens/loginIcon.png';
-
-
 
 const Home = ({text}) => {
 
@@ -17,64 +14,68 @@ return (
 
     <main>
 
-        <div className={styles.flag}>
+      <div className={styles.flag}>
 
-          <div className={styles.login}>
-            <div>
+        <div className={styles.login}>
+
+          <div>
             <img onClick={()=> navigation.push('/Login')} alt='/logo' src={logo}></img>
-            </div>
           </div>
 
-          <div className={styles.text_button}>
+        </div>
+
+        <div className={styles.text_button}>
+
+          <div className={styles.elements}>
+            <p className={styles.blink}>Crie uma conta</p>
+          </div>
+
             <div className={styles.elements}>
-              <p className={styles.blink}>Crie uma conta</p>
+              <Button onClick={()=> navigation.push('/Cadastro')} text="Aqui" className={styles_b.button_h}/>
             </div>
 
-              <div className={styles.elements}>
-                <Button onClick={()=> navigation.push('/Cadastro')} text="Aqui" className={styles_b.button_h}/>
-              </div>
-          </div>
-
         </div>
 
-      
-        <div className={styles.text_two}>
+      </div>
+
+      <div className={styles.text_two}>
         
-          <h1>Conectando pessoas pela arte</h1>
-          <p className={styles.writes_animation}>Crie sua conta e receba pré-atendimentos de sessões de tatuagem e solicitações de orçamento de todo o Brasil</p>
+        <h1>Conectando pessoas pela arte</h1>
+        <p className={styles.writes_animation}>Crie sua conta e receba pré-atendimentos de sessões de tatuagem e solicitações de orçamento de todo o Brasil</p>
       
-        </div>
+      </div>
 
-        <div className={styles.flag_artist}></div>
+      <div className={styles.flag_artist}></div>
         
-        <div className={styles.text_two}>
+      <div className={styles.text_two}>
 
-          <h3>Artistas que tatuam em todos os estilos</h3>
+        <h3>Artistas que tatuam em todos os estilos</h3>
 
-          <div style={{display: "flex", flexDirection: "column", alignItems:"flex-end"}}>
-          <Button onClick={()=> navigation.push('/Tatuadores')} text="Conheça nossos artistas" 
+        <div style={{display: "flex", flexDirection: "column", alignItems:"flex-end"}}>
+        <Button onClick={()=> navigation.push('/Tatuadores')} text="Conheça nossos artistas" 
           className={styles_b.button_quite}/>
-          </div>
-
         </div>
 
-        <div className={styles.flag_budget}></div>
+      </div>
+
+      <div className={styles.flag_budget}></div>
         
-        <div className={styles.text_two}>
+      <div className={styles.text_two}>
 
-          <h3>Conheça um pouco da nossa arte</h3>
+        <h3>Conheça um pouco da nossa arte</h3>
 
-          <div style={{display: "flex", flexDirection: "column", alignItems:"flex-end"}}>
-            <Button onClick={()=> navigation.push('/Atendimentos')} text="Faça um orçamento" className={styles_b.button_end}/>
-          </div>
-          
+        <div style={{display: "flex", flexDirection: "column", alignItems:"flex-end"}}>
+          <Button onClick={()=> navigation.push('/Atendimentos')} text="Faça um orçamento" className={styles_b.button_end}/>
         </div>
+          
+      </div>
+      
 
     </main>
     
   
   )
-   
+
 }
 
 
