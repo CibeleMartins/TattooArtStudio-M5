4d43180fa99e5627afaa299d1a3377d44c9a6f51
Ivy Richa <ivyricha@gmail.com>
2022-04-27 21:08:15 -0300

    :sparkles: feat: formulário pagina de atendimentos

--- src/pages/EditarAtendimentos.jsx
+++ src/pages/EditarAtendimentos.jsx
@@ -46,5 +46,71 @@ const getAtendimento = (id) => {
     }
 
     isedit()
+    return ( 
+        <div>
+            <Header/>
+   <form onSubmit={handleSubmit(editable? editar : agendar )}>
+           <input
+             type="text"
+             name="nome"
+             value={editable? Atendimento.nome : undefined}
+             {...register("nome")}
+             placeholder="Insira seu nome"
+           />
+   
+           <input
+             type="email"
+             name="email"
+             value={editable? Atendimento.email : undefined}
+             {...register("email")}
+             placeholder="Insira seu email"
+           />
+   
+           <input
+             type="text"
+             name="telefone"
+             value={editable? Atendimento.telefone : undefined}
+             {...register("telefone")}
+             placeholder="Insira seu Telefone"
+           />
+   
+           <input
+             type="text"
+             name="cidade"
+             value={editable? Atendimento.cidade : undefined}
+             {...register("cidade")}
+             placeholder="Insira sua Cidade"
+           />
+   
+           <input
+             type="text"
+             name="estado"
+             value={editable? Atendimento.estado : undefined}
+             {...register("estado")}
+             placeholder="Insira seu Estado"
+           />
+           <input
+             type="text"
+             name="data"
+             value={editable? Atendimento.data : undefined}
+             {...register("data")}
+             placeholder="Insira a data que você gostaria"
+           />
+           <input
+             type="text"
+             name="horário"
+             value={editable? Atendimento.horário : undefined}
+             {...register("horário")}
+             placeholder="Insira o horário que você gostaria"
+           />
+   
+           <button type="submit"> {editable? "Editar" : "Agendar"}</button>
+         </form>
+         <Footer/>
+         </div>
+   
+    )
 
 };
+
+export default CreatEditForm; 
