5bc9988534181feed5de2a3349f9bc5de98ecec9
Cibele Martins <93346472+CibeleMartins@users.noreply.github.com>
2022-04-29 01:47:03 -0300

    :recycle: feat: Refatorando pages Tatuadores e Loader
    Refatorou a pages Tatuadores e uma margin no Loader

--- src/pages/Tatuadores.jsx
+++ src/pages/Tatuadores.jsx
@@ -1,3 +1,6 @@
+import Button from '../components/Button/Button'
+import styles_b from '../components/Button/Button.module.css'
+
 import styles from './Tatuadores.module.css';
 import artist from "../assets/imagens/artist.png"
 import artist1 from "../assets/imagens/artist1.png"
@@ -16,20 +19,41 @@ function Tatuadores() {
 
             <div className={styles.cards}>
 
-                <img className={styles.imgArtists} src={artist}  alt='artist'/>
+                <div className={styles.divInfos}>
+                    <h4>Carlos Eduardo</h4>
+                    <br></br>
+                    <p>25 anos</p>
+                    <br></br>
+                    <p>PONTILHISMO · OLD SCHOOL · GEOMÉTRICO</p>
+                    <br></br>
+                    <p><strong>Atende em São Paulo e Rio de Janeiro</strong></p>
+                </div>
 
+                <img className={styles.imgArtists} src={artist}  alt='artist'/>
+                
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
+            
             </div>
+
 
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist1} alt='artist'/>
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
+            
 
             </div>
 
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist2} alt='artist'></img>
-
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
             </div>
 
         </div>
@@ -41,18 +65,27 @@ function Tatuadores() {
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist4} alt='artist'></img>
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
 
             </div>
 
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist3} alt='artist'></img>
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
 
             </div>
 
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist2} alt='artist'></img>
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
 
             </div>
 
@@ -65,18 +98,25 @@ function Tatuadores() {
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist2} alt='artist'></img>
-
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
             </div>
 
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist2} alt='artist'></img>
-
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
             </div>
 
             <div className={styles.cards}>
 
                 <img className={styles.imgArtists} src={artist2} alt='artist'></img>
+                <div className={styles.buttonBox}>
+                <Button className={styles_b.button_h} text='Agendar'/>
+                </div>
 
             </div>
 
