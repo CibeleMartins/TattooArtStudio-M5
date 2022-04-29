9ff01c8858e96c96d0d2dd277fde16595eb92b46
Ivy Richa <ivyricha@gmail.com>
2022-04-28 21:49:44 -0300

    fix: Alteração na página de tatuadores

--- src/pages/Tatuadores.jsx
+++ src/pages/Tatuadores.jsx
@@ -2,6 +2,7 @@ import styles from "./Tatuadores.module.css"
 import artist from "../assets/imagens/artist.png"
 import artist2 from "../assets/imagens/artist2.png"
 import artist3 from "../assets/imagens/artist3.png"
+import artist4 from "../assets/imagens/artist4.png"
 
 function Tatuadores() {
     return(
@@ -19,7 +20,7 @@ function Tatuadores() {
             <div className={styles.titulo}>
                <h3>Paulo André</h3>
                <div className={styles.body }>
-               <p>26 anos - São paulo - 4 anos de profissão</p>
+               <p>26 anos - São Paulo - 6 anos de profissão</p>
                </div>
             </div>
         <div className={styles.bnt}>
@@ -36,9 +37,9 @@ function Tatuadores() {
             <div> 
                 <img src={artist2} alt="tatuadora"></img>
             <div className={styles.titulo}>
-               <h3>Felipe Oliveira</h3>
+               <h3>Vanessa Oliveira</h3>
                <div className={styles.body }>
-               <p>29 anos - Rio de Janeiro - 7 anos de profissão</p>
+               <p>29 anos - Rio de Janeiro - 4 anos de profissão</p>
                </div>
             </div>
         <div className={styles.bnt}>
@@ -53,11 +54,11 @@ function Tatuadores() {
 
         <div className={styles.container}>
             <div> 
-                <img src={artist3} alt="tatuador"></img>
+                <img src={artist4} alt="tatuador"></img>
             <div className={styles.titulo}>
-               <h3>Kiko Pontes</h3>
+               <h3>Julio Pontes</h3>
                <div className={styles.body }>
-               <p>34 anos - são paulo - 15 anos de profissão</p>
+               <p>34 anos - São Paulo - 15 anos de profissão</p>
                </div>
 
             <div>
@@ -79,7 +80,7 @@ function Tatuadores() {
             <div className={styles.titulo}>
                <h3>Kiko Pontes</h3>
                <div className={styles.body }>
-               <p>34 anos - são paulo - 15 anos de profissão</p>
+               <p>27 anos - Belo Horizonte - 3 anos de profissão</p>
                </div>
 
             <div>
@@ -95,17 +96,6 @@ function Tatuadores() {
         </div>
     </div>
 
-        <div className={styles.tituloFim}>
-               <h1>Quem somos</h1>
-               <p>  Fundado em 1997, o Tattoo Art Studio atualmente é considerado um
-                    dos mais conhecidos e respeitados estúdios 
-                    de tatuagem do Brasil. conquistando nosso espaço ao longo de mais
-                    de 20 anos de atuação no segmento de tatuagens. Além dos prêmios
-                    conquistados individualmente por cada tatuador que forma 
-                    a nossa equipe, também ficamos muito satisfeitos pelo reconhecimento
-                    de nossos clientes e felizes com a dedicação que temos em 
-                    eternizar em suas peles os sonhos e emoções unicamente incríveis.</p>
-        </div>
         </div>
         </main>
 
