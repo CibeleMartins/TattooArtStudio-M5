f75a8db6697ff0156051f070fe845a9a46927b09
Cibele Martins <93346472+CibeleMartins@users.noreply.github.com>
2022-04-29 01:00:41 -0300

    feat: Refatorando a pages Tatuadores

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Atendimentos from './pages/Atendimentos'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Container from './pages/layout/Container/Container.jsx';
+import stylesC from './pages/layout/Container/Container.module.css'
 import './App.css';
 
 function App() {
@@ -41,7 +42,7 @@ function App() {
 
         {!display && <Header/>}
         
-        <Container customClass= "min-height">
+        <Container>
           <Switch>
             <Route exact path="/">
               <Home />
--- src/pages/Tatuadores.jsx
+++ src/pages/Tatuadores.jsx
@@ -1,103 +1,88 @@
-import styles from "./Tatuadores.module.css"
+import styles from './Tatuadores.module.css';
 import artist from "../assets/imagens/artist.png"
+import artist1 from "../assets/imagens/artist1.png"
 import artist2 from "../assets/imagens/artist2.png"
 import artist3 from "../assets/imagens/artist3.png"
 import artist4 from "../assets/imagens/artist4.png"
 
 function Tatuadores() {
-    return(
 
-        <main>  
-            
-        <div className={styles.tituloIn}>
-               <h1>Alguns dos nossos Artistas</h1>
+    return (
+
+    <div className={styles.container}>  
+        
+        {/* first division */}
+        <div className={styles.firstDivision}>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist}  alt='artist'/>
+
+            </div>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist1} alt='artist'/>
+
+            </div>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist2} alt='artist'></img>
+
+            </div>
+
+        </div>
+
+        {/* second division */}
+
+        <div className={styles.secondDivision}>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist4} alt='artist'></img>
+
+            </div>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist3} alt='artist'></img>
+
+            </div>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist2} alt='artist'></img>
+
+            </div>
+
+        </div>
+
+        {/* third division */}
+
+        <div className={styles.thirdDivision}>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist2} alt='artist'></img>
+
+            </div>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist2} alt='artist'></img>
+
+            </div>
+
+            <div className={styles.cards}>
+
+                <img className={styles.imgArtists} src={artist2} alt='artist'></img>
+
+            </div>
+
         </div>
         
-    <div className={styles.cards}>
-        <div className={styles.container}>
-            <div className={styles.imagem}> 
-                <img src={artist} alt="tatuador"></img>
-            <div className={styles.titulo}>
-               <h3>Paulo André</h3>
-               <div className={styles.body }>
-               <p>26 anos - São Paulo - 6 anos de profissão</p>
-               </div>
-            </div>
-        <div className={styles.bnt}>
-            <button className={styles.button}>
-                <a href="./Contato">
-                    Contato
-                </a>
-            </button>
-        </div>
-        </div>
-        </div>
-
-        <div className={styles.container}>
-            <div> 
-                <img src={artist2} alt="tatuadora"></img>
-            <div className={styles.titulo}>
-               <h3>Vanessa Oliveira</h3>
-               <div className={styles.body }>
-               <p>29 anos - Rio de Janeiro - 4 anos de profissão</p>
-               </div>
-            </div>
-        <div className={styles.bnt}>
-            <button className={styles.button}>
-                <a href="./Contato">
-                    Contato
-                </a>
-            </button>
-        </div>
-        </div>
-        </div>
-
-        <div className={styles.container}>
-            <div> 
-                <img src={artist4} alt="tatuador"></img>
-            <div className={styles.titulo}>
-               <h3>Julio Pontes</h3>
-               <div className={styles.body }>
-               <p>34 anos - São Paulo - 15 anos de profissão</p>
-               </div>
-
-            <div>
-            </div>
-        <div className={styles.bnt}>
-            <button className={styles.button}>
-                <a href="./Contato">
-                    Contato
-                </a>
-            </button>
-        </div>
-        </div>
-        </div>
     </div>
-
-    <div className={styles.container}>
-            <div> 
-                <img src={artist3} alt="tatuador"></img>
-            <div className={styles.titulo}>
-               <h3>Kiko Pontes</h3>
-               <div className={styles.body }>
-               <p>27 anos - Belo Horizonte - 3 anos de profissão</p>
-               </div>
-
-            <div>
-            </div>
-        <div className={styles.bnt}>
-            <button className={styles.button}>
-                <a href="./Contato">
-                    Contato
-                </a>
-            </button>
-        </div>
-        </div>
-        </div>
-    </div>
-
-        </div>
-        </main>
 
         
     )
